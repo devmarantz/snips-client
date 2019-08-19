58a930338429a56025d200a69cd6b50ea3f6cd02
Marantz <marantz@cala-am02059581.attlocal.net>
2019-08-19 14:15:32 -0700

    Add event listener on search bar

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 import SNIPS from './snippet-data.js';
-import { renderSnips, highlightSnips } from './lib/snippets.js';
-import search from './lib/search.js';
+import { renderSnips} from './lib/snippets.js';
+import {search} from './lib/search.js';
 
 renderSnips(SNIPS);
 
-const searchForm = document.getElementById('search-bar');
-const searchText = document.getElementById('search-text');
-
-searchForm.addEventListener('submit', event => {
-  event.preventDefault();
-  search(searchText.value);
-});
-
-// hljs.initHighlightingOnLoad();
-highlightSnips();
+document.getElementById('search-bar').addEventListener('submit', search);
