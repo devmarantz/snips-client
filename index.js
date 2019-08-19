e713e65c6ac36d3cb5168e9a89ba268f19836289
Marantz <marantz@cala-am02059581.attlocal.net>
2019-08-19 11:29:29 -0700

    Update snippets html and index js

--- index.js
+++ index.js
@@ -3,4 +3,13 @@ import { renderSnips } from './lib/snippets.js';
 import search from './lib/search.js';
 
 renderSnips(SNIPS);
+
+const searchForm = document.getElementById('search-bar');
+const searchText = document.getElementById('search-text');
+
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  search(searchText.value);
+});
+
 hljs.initHighlightingOnLoad();
