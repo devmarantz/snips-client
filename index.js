46b2151527919768009c537819c065447ba48556
Marantz <devonmarantz@gmail.com>
2019-08-20 11:36:38 -0700

    Implement fetchSnippets function on initial load

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import SNIPS from './snippet-data.js';
-import { renderSnips} from './lib/snippets.js';
+import { renderSnips, fetchSnippets } from './lib/snippets.js';
 import {search} from './lib/search.js';
 
-renderSnips(SNIPS);
+fetchSnippets().then(snips => {
+    renderSnips(snips);
+});
+
 
 document.getElementById('search-bar').addEventListener('submit', search);
